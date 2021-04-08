6372c8309c743aee5d169bd0c2b34723486e35d4
cpbram2011 <cpbram2011@gmail.com>
2021-04-07 19:24:21 -0500

    start routes

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
-// const db = mongoose
+const db = require("./config/keys").mongoURI;
+
+mongoose
+    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then( () => console.log('mongo connected'))
+    .catch(err => console.log(err))
 
 app.get("/", (req, res) => {
     res.send('Test Success')
