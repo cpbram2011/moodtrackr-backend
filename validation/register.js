8df936ceb93ed060a4bd12b5830e68f7f7414282
cpbram2011 <cpbram2011@gmail.com>
2021-04-16 06:19:27 -0500

    minor tweaks

--- validation/register.js
+++ validation/register.js
@@ -4,18 +4,18 @@ const validText = require('./valid-text');
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
-  data.name = validText(data.name) ? data.name : '';
+  data.username = validText(data.username) ? data.username : '';
   data.email = validText(data.email) ? data.email : '';
   data.password = validText(data.password) ? data.password : '';
   data.password2 = validText(data.password2) ? data.password2 : '';
 
   
-  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = 'Name must be more than 2 characters and less than 30';
+  if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
+    errors.username = 'Name must be more than 2 characters and less than 30';
   }
 
-  if (Validator.isEmpty(data.name)) {
-    errors.name = 'Do not forget your name!';
+  if (Validator.isEmpty(data.username)) {
+    errors.username = 'Do not forget your username!';
   }
 
   if (Validator.isEmpty(data.email)) {
