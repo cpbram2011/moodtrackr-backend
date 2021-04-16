9d47a921dd9f73c8eba3e0521aa722ed2eb505d2
cpbram2011 <cpbram2011@gmail.com>
2021-04-16 07:51:43 -0500

    fix auth timeout

--- routes/api/users.js
+++ routes/api/users.js
@@ -87,7 +87,7 @@ router.post("/login", (req, res) => {
       if (isMatch) {
         const payload = { id: user.id, email: user.email };
 
-        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+        jwt.sign(payload, keys.secretOrKey, { expiresIn: "1d" }, (err, token) => {
           res.json({
             success: true,
             token: "Bearer " + token
